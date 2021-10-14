feb63e5f638ae3fe79d5ef35a938fdb5ac30a634
miwasuke <miwa.ryo14@gmail.com>
2021-10-14 19:58:14 +0900

    web追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
