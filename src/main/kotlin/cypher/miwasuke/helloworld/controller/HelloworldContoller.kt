695aa0c316928379987e6ad3edf8d44b73780963
miwasuke <miwa.ryo14@gmail.com>
2021-10-28 19:50:16 +0900

    名前を返すAPIを実装

--- src/main/kotlin/cypher/miwasuke/helloworld/controller/HelloworldContoller.kt
+++ src/main/kotlin/cypher/miwasuke/helloworld/controller/HelloworldContoller.kt
@@ -1,6 +1,7 @@
 package cypher.miwasuke.helloworld.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -8,4 +9,9 @@ class HelloworldController {
 
     @GetMapping("/")
     fun hello(): String = "hello world"
+
+    @GetMapping("/hello")
+    fun helloName(
+        @RequestParam name: String
+    ): String = ("Hello, $name")
 }
