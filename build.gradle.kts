4ff020933d7ea5c221db4ea81ee481220360b7d7
miwasuke <miwa.ryo14@gmail.com>
2021-11-07 18:33:55 +0900

    不要なimplementation削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks.withType<KotlinCompile> {
